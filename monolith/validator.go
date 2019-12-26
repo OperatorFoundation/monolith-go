f1cf5dd51c86ea42deedf4329c1272dd3db7cea5
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2019-12-26 14:38:46 -0600

    Changed validator to handle incomplete cases

--- monolith/validator.go
+++ monolith/validator.go
@@ -2,50 +2,79 @@ package monolith
 
 import "github.com/deckarep/golang-set"
 
-type Validateable interface {
-	Validate(bs []byte) ([]byte, bool)
+type Validity int
+
+const (
+	Valid Validity = iota
+	Invalid
+	Incomplete
+)
+
+func (v Validity) String() string {
+	return [...]string{"Valid", "Invalid", "Incomplete"}[v]
 }
 
-func (desc Description) Validate(bs []byte) ([]byte, bool) {
+type Validateable interface {
+	Validate(bs []byte) ([]byte, Validity)
+}
+
+func (desc Description) Validate(bs []byte) ([]byte, Validity) {
 	bs2 := bs
-	valid := true
+	valid := Valid
 	for _, part := range desc.Parts {
 		bs2, valid = part.Validate(bs2)
-		if !valid {
-			return bs2, false
+		switch valid {
+		case Valid:
+			continue
+		case Invalid:
+			return bs2, Invalid
+		case Incomplete:
+			return bs2, Incomplete
+		default:
+			return bs2, Invalid
 		}
 	}
 
-	return bs2, true
+	return bs2, Valid
 }
 
-func (part BytesPart) Validate(bs []byte) ([]byte, bool) {
+func (part BytesPart) Validate(bs []byte) ([]byte, Validity) {
 	bs2 := bs
-	valid := true
+	valid := Valid
 
 	for index := 0; index < len(part.Items); index++ {
 		bs2, valid = part.Items[index].Validate(bs2)
-		if !valid {
-			return bs2, false
+		switch valid {
+		case Valid:
+			continue
+		case Invalid:
+			return bs2, Invalid
+		case Incomplete:
+			return bs2, Incomplete
+		default:
 		}
 	}
 
-	return bs2, true
+	return bs2, Valid
 }
 
-func (bt FixedByteType) Validate(bs []byte) ([]byte, bool) {
+func (bt FixedByteType) Validate(bs []byte) ([]byte, Validity) {
 	if len(bs) == 0 {
-		return bs, false
+		return bs, Incomplete
 	}
 
 	b, bs2 := bs[0], bs[1:]
 
-	return bs2, b == bt.Byte
+	if b == bt.Byte {
+		return bs2, Valid
+	} else {
+		return bs2, Invalid
+	}
 }
 
-func (bt EnumeratedByteType) Validate(bs []byte) ([]byte, bool) {
+func (bt EnumeratedByteType) Validate(bs []byte) ([]byte, Validity) {
 	if len(bs) == 0 {
-		return bs, false
+		return bs, Incomplete
 	}
 
 	b, bs2 := bs[0], bs[1:]
@@ -56,22 +85,26 @@ func (bt EnumeratedByteType) Validate(bs []byte) ([]byte, bool) {
 	}
 
 	set := mapset.NewSetFromSlice(options)
-	return bs2, set.Contains(b)
+	if set.Contains(b) {
+		return bs2, Valid
+	} else {
+		return bs2, Invalid
+	}
 }
 
-func (bt RandomByteType) Validate(bs []byte) ([]byte, bool) {
+func (bt RandomByteType) Validate(bs []byte) ([]byte, Validity) {
 	if len(bs) == 0 {
-		return bs, false
+		return bs, Incomplete
 	}
 
 	_, bs2 := bs[0], bs[1:]
 
-	return bs2, true
+	return bs2, Valid
 }
 
-func (bt RandomEnumeratedByteType) Validate(bs []byte) ([]byte, bool) {
+func (bt RandomEnumeratedByteType) Validate(bs []byte) ([]byte, Validity) {
 	if len(bs) == 0 {
-		return bs, false
+		return bs, Incomplete
 	}
 
 	b, bs2 := bs[0], bs[1:]
@@ -82,6 +115,11 @@ func (bt RandomEnumeratedByteType) Validate(bs []byte) ([]byte, bool) {
 	}
 
 	set := mapset.NewSetFromSlice(options)
-	return bs2, set.Contains(b)
+
+	if set.Contains(b) {
+		return bs2, Valid
+	} else {
+		return bs2, Invalid
+	}
 }
 
